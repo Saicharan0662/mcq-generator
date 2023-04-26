b3bf1435a3315159e27e42ab40e0ca27b4e7c154
sai charan <saicharan0662@gmail.com>
2023-04-26 22:30:09 +0530

    minor bugs

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,10 +4,11 @@ import LinearProgress from '@mui/material/LinearProgress';
 import { Link } from 'react-router-dom';
 
 const Navbar = ({ text = 'Home', clickHandler, isLoading = false }) => {
+    const user = JSON.parse(localStorage.getItem('user'))
     return (
         <div className='sticky top-0' style={{ zIndex: 2 }}>
             <div className='h-12 bg-blue-700 text-white flex justify-between items-center px-6'>
-                <Link to={'/dashboard'}>
+                <Link to={user ? '/dashboard' : '/'}>
                     <div className='font-bold cursor-pointer'>Question Generator</div>
                 </Link>
                 <Button
